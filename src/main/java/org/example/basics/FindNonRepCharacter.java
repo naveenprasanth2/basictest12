7fb93b0da15cdbc0208baee1974da8567db219a7
naveen prasanth <developer.hotel@bookany.in>
2024-04-09 17:58:07 +0530

    first non rep character

---
+++ src/main/java/org/example/basics/FindNonRepCharacter.java
@@ -0,0 +1,14 @@
+package org.example.basics;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class FindNonRepCharacter {
+    public static void main(String[] args) {
+        String a = "is it your first company";
+       List<String> test =  Stream.of(a.replaceAll("\\s", "").split("")).toList();
+                test.stream().filter(x -> Collections.frequency(test, x) == 1)
+                        .findFirst().ifPresentOrElse(System.out::println, ()-> System.out.println("not present"));
+    }
+}
