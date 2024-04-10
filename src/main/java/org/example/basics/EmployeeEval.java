826f2fa858f883f6cd288ff2e97914e912894fbe
naveen prasanth <naveenprasanth12@gmail.com>
2024-04-10 21:37:55 +0530

    count employee based on age

--- src/main/java/org/example/basics/EmployeeEval.java
+++ src/main/java/org/example/basics/EmployeeEval.java
@@ -1,6 +1,12 @@
 package org.example.basics;
 
+import org.example.person.Person;
+
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class EmployeeEval {
     public static void main(String[] args) {
@@ -22,5 +28,11 @@ public class EmployeeEval {
 
         employees.stream().filter(x -> x.getDevices().contains("galaxy")).map(Employee::getName).findFirst()
                 .ifPresentOrElse(System.out::println, () -> System.out.println("No employee found"));
+
+        List<Map.Entry<Integer, Long>> test = employees.stream()
+                .collect(Collectors.groupingBy(Employee::getAge, Collectors.counting()))
+                .entrySet().stream().toList();
+
+        System.out.println(test);
     }
 }
