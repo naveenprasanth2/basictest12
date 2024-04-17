ebf44ff964a5f33bfed5fc36c17ed8a64b096b3c
naveen prasanth <naveenprasanth12@gmail.com>
2024-04-17 19:20:06 +0530

    break singleton

---
+++ src/main/java/org/example/singleton1/Singleton.java
@@ -0,0 +1,24 @@
+package org.example.singleton1;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Singleton {
+    private static volatile Singleton singleton;
+    private String name;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+        if (singleton == null) synchronized (Singleton.class) {
+            if (singleton == null) {
+                singleton = new Singleton();
+            }
+        }
+        return singleton;
+    }
+}
