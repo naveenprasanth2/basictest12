6bb325b4b6804553d57738634c9ea143c858902e
naveen prasanth <naveenprasanth12@gmail.com>
2024-04-20 17:49:04 +0530

    number guessing game

--- src/main/java/org/example/challenges/NumberGuessingGame.java
+++ src/main/java/org/example/challenges/NumberGuessingGame.java
@@ -1,7 +1,34 @@
 package org.example.challenges;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class NumberGuessingGame {
     public static void main(String[] args) {
-        
+        Random random = new Random();
+        int tryCount = 0;
+        boolean broke = false;
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Please enter the from range");
+        int from = scanner1.nextInt();
+        System.out.println("Please enter the to range");
+        int to = scanner1.nextInt();
+        int randomNumber = random.nextInt(from, to + 1);
+        while (!broke) {
+            System.out.println(STR."Please enter your guess between \{from} - \{to}:");
+            Scanner scanner = new Scanner(System.in);
+            int playerGuess = scanner.nextInt();
+            tryCount++;
+            if (playerGuess == randomNumber) {
+                System.out.println("Its correct");
+                System.out.println(STR."It took \{tryCount} tries for you to guess");
+                broke = true;
+            } else if (playerGuess > randomNumber) {
+                System.out.println("Its a smaller number");
+            } else {
+                System.out.println("Its a higher number");
+            }
+        }
+
     }
 }
