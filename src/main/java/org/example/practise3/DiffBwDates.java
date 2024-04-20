25a39250526092abb3de1f7e59b932b2947610b9
naveen prasanth <naveenprasanth12@gmail.com>
2024-04-20 10:03:28 +0530

    dates change

---
+++ src/main/java/org/example/practise3/DiffBwDates.java
@@ -0,0 +1,11 @@
+package org.example.practise3;
+
+import java.time.LocalDateTime;
+import java.time.temporal.TemporalAmount;
+
+public class DiffBwDates {
+    public static void main(String[] args) {
+        LocalDateTime localDateTime = LocalDateTime.now();
+        LocalDateTime localDateTime1 = localDateTime.now();
+    }
+}
